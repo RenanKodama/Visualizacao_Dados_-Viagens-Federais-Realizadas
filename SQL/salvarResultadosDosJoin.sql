6f7ea7bc641b6472b148147f088ff4a59cae8ba1
clodoaldoBasaglia <clodoaldofonseca92@gmail.com>
2018-09-13 05:04:04 -0300

    separando os dados em outros arquivos

--- SQL/salvarResultadosDosJoin.sql
+++ SQL/salvarResultadosDosJoin.sql
@@ -1,23 +1,23 @@
 SELECT * FROM Pagamento 
 INNER JOIN idsImportantes ON Pagamento.idProcesso = idsImportantes.idProcesso
-INTO OUTFILE '/home/clodoaldo/UTFPR/2018-2/VisualisacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/pagamento.csv' 
+INTO OUTFILE '/var/lib/mysql-files/pagamento.csv' 
 CHARACTER SET latin1 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 SELECT * FROM Passagem 
 INNER JOIN idsImportantes ON Passagem.idProcesso = idsImportantes.idProcesso
-INTO OUTFILE '/home/clodoaldo/UTFPR/2018-2/VisualisacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/passagem.csv' 
+INTO OUTFILE '/var/lib/mysql-files/passagem.csv' 
 CHARACTER SET latin1 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 SELECT * FROM Trecho 
 INNER JOIN idsImportantes ON Trecho.idProcesso = idsImportantes.idProcesso
-INTO OUTFILE '/home/clodoaldo/UTFPR/2018-2/VisualisacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/trecho.csv' 
+INTO OUTFILE '/var/lib/mysql-files/trecho.csv' 
 CHARACTER SET latin1 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 SELECT * FROM Viagem 
 INNER JOIN idsImportantes ON Viagem.idProcesso = idsImportantes.idProcesso
-INTO OUTFILE '/home/clodoaldo/UTFPR/2018-2/VisualisacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/viagem.csv' 
+INTO OUTFILE '/var/lib/mysql-files/viagem.csv' 
 CHARACTER SET latin1 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"' LINES TERMINATED BY '\n';
