56bbbe9c4339eeabe7f6337bf27e48e0c46e2f45
Clodoaldo Basaglia <clodoaldofonseca92@gmail.com>
2018-10-17 22:59:07 -0300

    problemas com o R

--- R/streamgraph.R
+++ R/streamgraph.R
@@ -1,13 +1,15 @@
 # Library
 setwd("~/UTF/Visualizacao_Dados_Viagens-Federais-Realizadas/R")
+# Library
 library(streamgraph)
 
 # Create data:
 year=rep(seq(1,12) , each=1)
-name=rep(letters[1:10] , 27)
+name=rep(letters[1:12] , 1)
 value=sample( seq(0,1,0.0001) , length(year))
 data=data.frame(year, name, value)
 
 # Stream graph with a legend
 streamgraph(data, key="name", value="value", date="year" )%>%
   sg_legend(show=TRUE, label="names: ")
+
