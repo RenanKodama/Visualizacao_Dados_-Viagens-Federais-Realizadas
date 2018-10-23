3161897c99342b8222fc194ad777f57cee58d583
Clodoaldo Basaglia <clodoaldofonseca92@gmail.com>
2018-10-23 01:28:16 -0300

    ainda apanhando do R

--- R/streamgraph.R
+++ R/streamgraph.R
@@ -1,15 +1,14 @@
 # Library
-setwd("~/UTF/Visualizacao_Dados_Viagens-Federais-Realizadas/R")
+setwd("~/UTFPR/Visualizacao_Dados_Viagens-Federais-Realizadas/R")
 # Library
 library(streamgraph)
 
 # Create data:
-year=rep(seq(1,12) , each=1)
-name=rep(letters[1:12] , 1)
+year=rep(seq(1990,2016) , each=10)
+name=rep(letters[1:10] , 27)
 value=sample( seq(0,1,0.0001) , length(year))
 data=data.frame(year, name, value)
 
 # Stream graph with a legend
-streamgraph(data, key="name", value="value", date="year" )%>%
-  sg_legend(show=TRUE, label="names: ")
-
+streamgraph(data, key="name", value="value", date="year" )
+  #sg_legend(show=TRUE, label="names: ")
