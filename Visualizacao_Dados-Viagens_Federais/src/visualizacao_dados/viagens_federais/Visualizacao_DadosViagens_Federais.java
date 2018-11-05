01fd05f24a7d283d05e1293265eb46628b747b82
Clodoaldo Basaglia <clodoaldofonseca92@gmail.com>
2018-11-05 02:01:55 -0200

    refiz a base de viagens e peguei os motivos dos 12 meses, comecei um codigo em java mas não consegui fazer então fiz um em python de 10 linhas que resolveu tudo, PYthon e teamo/odeio

--- Visualizacao_Dados-Viagens_Federais/src/visualizacao_dados/viagens_federais/Visualizacao_DadosViagens_Federais.java
+++ Visualizacao_Dados-Viagens_Federais/src/visualizacao_dados/viagens_federais/Visualizacao_DadosViagens_Federais.java
@@ -1,11 +1,23 @@
-
 package visualizacao_dados.viagens_federais;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Visualizacao_DadosViagens_Federais {
 
     public static void main(String[] args) {
-        System.out.println("oi");
+        ManipulaArquivo ma = new ManipulaArquivo();
+        List<String> mesSemStopWords = new ArrayList<String>();
+        List<String> stopWords = ma.abrirArquivo("/home/clodoaldo/UTFPR/VisualizacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/Cloud Word Motivos/stopwords.txt");
+        List<String> mesMotivo = ma.abrirArquivo("/home/clodoaldo/UTFPR/VisualizacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/Cloud Word Motivos/MES_1.csv");
+        for (int i = 1; i < mesMotivo.size(); i++) {
+            for (int j = 0; j < stopWords.size(); j++) {
+                mesSemStopWords.add(mesMotivo.get(i).replace(stopWords.get(j), " "));
+                System.out.println(mesMotivo.get(i) + " " + stopWords.get(j));
+            }
+            break;
+        }
+        ma.salvarArquivo("/home/clodoaldo/UTFPR/VisualizacaoDados/Visualizacao_Dados_Viagens-Federais-Realizadas/Cloud Word Motivos/semStop_1.txt", mesSemStopWords);
     }
-    
+
 }
